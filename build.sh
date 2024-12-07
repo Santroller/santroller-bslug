847d087693fb471c0e69abf46a3b789c7a3c0ff3
Sanjay Govind <sanjay.govind9@gmail.com>
2024-12-08 01:25:09 +1300

    add missing symbols

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+make
+rm -rf sd.zip
+cp bin/santroller-bslug.mod bslug/modules
+cp -rf symbols bslug/symbols
+zip -r sd.zip bslug 
+cp ../brainslug-wii/bin/boot.dol RGHE52.dol
+zip sd.zip RGHE52.dol
+rm RGHE52.dol
